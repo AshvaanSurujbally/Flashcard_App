c7bfd12cc649d6b1618a8f43ce0c56f97240e1b7
Ashvaan <st10468080@vcconnect.edu.za>
2025-05-06 19:54:49 +0200

    Added the unversioned files that was not tracked by github changed some code in the game activity and added the two arrays but not completed

--- app/src/main/java/vcmsa/ashvaan12me/flashcard_app/gameMenu.kt
+++ app/src/main/java/vcmsa/ashvaan12me/flashcard_app/gameMenu.kt
@@ -1,6 +1,8 @@
 package vcmsa.ashvaan12me.flashcard_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,9 +19,36 @@ class gameMenu : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //first we delare the variables needed
+        val btnStartGame = findViewById<Button>(R.id.btnStartGame)
+        val btnExit = findViewById<Button>(R.id.btnExit)
+        val btnHistory = findViewById<Button>(R.id.btnHistory)
+
+        //now we get the username from the main activity and display it in the game menu page
+        // AI helped to with idea of how to get the username from the main activity but coding was my own work.
         val username = intent.getStringExtra("username")
         val tvUserGreeting = findViewById<TextView>(R.id.tvUserGreeting)
-        tvUserGreeting.text = "Hello, $username"
+        tvUserGreeting.text = "ARE YOU READY TO PLAY $username"
+
+        //now we set the exit button to take the user back to the main activity(the login page)
+        btnExit.setOnClickListener {
+            finish()
+        }
+            //set the start game button to take the user to the in game play page
+            btnStartGame.setOnClickListener {
+                val intent = Intent(this, inGamePlay::class.java)
+                startActivity(intent)
+            }
+                //set the history button to take the user to the review page
+                btnHistory.setOnClickListener {
+                    val intent = Intent(this, reviewPage::class.java)
+                    startActivity(intent)
+                }
+
+
+
+
+
 
     }
 }
